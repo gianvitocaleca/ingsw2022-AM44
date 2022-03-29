785ccff1e54243880e713aa0817f2d77147831e6
Paolo <10699692@polimi.it>
2022-03-29 22:22:34 +0200

    Test classes

--- src/test/java/it/polimi/ingsw/DiningRoomTest.java
+++ src/test/java/it/polimi/ingsw/DiningRoomTest.java
@@ -1,8 +1,17 @@
 package it.polimi.ingsw;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class DiningRoomTest {
+    private final int Max_cap = 50;
+    public DiningRoom dr;
+
+    @BeforeEach
+    public void Initialize() {
+        dr = new DiningRoom(Max_cap);
+
+    }
 
     @Test
     void addStudents() {
