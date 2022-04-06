7a73b0b15c208c691728645ffbd6f9f35f9c1729
Paolo <10699692@polimi.it>
2022-04-06 17:29:18 +0200

    Fixed maxCapcityBucket issue.

--- src/test/java/it/polimi/ingsw/studentsTests/StudentBucketTest.java
+++ src/test/java/it/polimi/ingsw/studentsTests/StudentBucketTest.java
@@ -4,6 +4,7 @@ import it.polimi.ingsw.model.enums.Creature;
 import it.polimi.ingsw.model.exceptions.StudentsOutOfStockException;
 import it.polimi.ingsw.model.students.Student;
 import it.polimi.ingsw.model.students.StudentBucket;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 
@@ -13,11 +14,14 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StudentBucketTest {
+    private int maxBucketCapacity = 130;
+
     @Test
     public void MaxCapacityBucket() {
+        StudentBucket.resetMap();
         StudentBucket bucket = StudentBucket.getInstance();
         List<Student> temp = new ArrayList<>();
-        while(true) {
+        while (true) {
             try {
                 temp.add(bucket.generateStudent());
             } catch (StudentsOutOfStockException ex) {
@@ -27,9 +31,9 @@ public class StudentBucketTest {
         int sum = 0;
         for (Creature c : Creature.values()) {
             sum += bucket.getNumberOfGeneratedStudentsByCreature(c);
-            assertEquals(bucket.getNumberOfGeneratedStudentsByCreature(c),26);
+            assertEquals(bucket.getNumberOfGeneratedStudentsByCreature(c), 26);
         }
-        assertEquals(sum, 130);
-        assertEquals(temp.size(),130);
+        assertEquals(maxBucketCapacity, sum);
+        assertEquals(maxBucketCapacity, temp.size());
     }
 }
