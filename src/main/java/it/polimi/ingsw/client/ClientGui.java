8004c2567e944e69ecf7627210f60c3831882fd7
Paolo <10699692@polimi.it>
2022-05-23 18:16:44 +0200

    JavaFx first implementation 2

--- src/main/java/it/polimi/ingsw/client/ClientGui.java
+++ src/main/java/it/polimi/ingsw/client/ClientGui.java
@@ -20,7 +20,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ClientGui extends Application {
-    private Stage stage;
     private ScreenController screenController;
 
     @Override
