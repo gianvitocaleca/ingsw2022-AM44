dee44e5477b48d826b94a4ff08799599f671b752
SabrinAzzi <sabrina.azzi@mail.polimi.it>
2022-06-06 12:20:55 +0200

    Implementation of Characters effect in GUI.

---
+++ src/main/java/it/polimi/ingsw/client/GUI/GUIPhases.java
@@ -0,0 +1,10 @@
+package it.polimi.ingsw.client.GUI;
+
+public enum GUIPhases {
+    SELECT_CREATURE,
+    SELECT_DESTINATION,
+    SELECT_CREATURE_FOR_CHARACTER,
+    SELECT_DESTINATION_ISLAND,
+    END;
+
+}
