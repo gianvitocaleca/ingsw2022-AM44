69c5377aa86989a81e237df4d5866f398deaeb48
Masterpavel2k <10699692@polimi.it>
2022-06-28 23:55:25 +0200

    added java doc

--- src/main/java/it/polimi/ingsw/network/server/states/LoginState.java
+++ src/main/java/it/polimi/ingsw/network/server/states/LoginState.java
@@ -15,12 +15,20 @@ public class LoginState {
 
     private final Map<Socket, Wizard> wizardMap;
 
+    /**
+     * Used to keep track of all the player's info during login phase
+     */
     public LoginState() {
         usernamesMap = new HashMap<>();
         colorMap = new HashMap<>();
         wizardMap = new HashMap<>();
     }
 
+    /**
+     * Used to get the username by the player's socket
+     * @param socket is the given socket
+     * @return is the username matching the given socket
+     */
     public String getUsername(Socket socket) {
         synchronized (this.usernamesMap) {
             while (!this.usernamesMap.containsKey(socket)) {
@@ -33,10 +41,19 @@ public class LoginState {
         }
     }
 
+    /**
+     * Used to remove the username matching the given socket
+     * @param socket is the socket to be removed
+     */
     public void removeUsername(Socket socket) {
         this.usernamesMap.remove(socket);
     }
 
+    /**
+     * Used to set the username for a given socket
+     * @param socket is the given socket
+     * @param username is the given username
+     */
     public void setUsername(Socket socket, String username) {
         synchronized (this.usernamesMap) {
             this.usernamesMap.put(socket, username);
@@ -44,6 +61,11 @@ public class LoginState {
         }
     }
 
+    /**
+     * Used to get the color of the given socket
+     * @param socket is the given socket
+     * @return is the color matching the given socket
+     */
     public Color getColor(Socket socket) {
         synchronized (this.colorMap) {
             while (!this.colorMap.containsKey(socket)) {
@@ -56,10 +78,19 @@ public class LoginState {
         }
     }
 
+    /**
+     * Used to remove the color matching the given socket
+     * @param socket is the socket to be removed
+     */
     public void removeColor(Socket socket) {
         this.colorMap.remove(socket);
     }
 
+    /**
+     * Used to set the color for a given socket
+     * @param socket is the given socket
+     * @param color is the given username
+     */
     public void setColor(Socket socket, Color color) {
         synchronized (this.colorMap) {
             this.colorMap.put(socket, color);
@@ -67,6 +98,11 @@ public class LoginState {
         }
     }
 
+    /**
+     * Used to get the wizard of the given socket
+     * @param socket is the given socket
+     * @return is the wizard matching the given socket
+     */
     public Wizard getWizard(Socket socket) {
         synchronized (this.wizardMap) {
             while (!this.wizardMap.containsKey(socket)) {
@@ -79,10 +115,19 @@ public class LoginState {
         }
     }
 
+    /**
+     * Used to remove the wizard matching the given socket
+     * @param socket is the socket to be removed
+     */
     public void removeWizard(Socket socket) {
         this.wizardMap.remove(socket);
     }
 
+    /**
+     * Used to set the wizard for a given socket
+     * @param socket is the given socket
+     * @param wizard is the given username
+     */
     public void setWizard(Socket socket, Wizard wizard) {
         synchronized (this.wizardMap) {
             this.wizardMap.put(socket, wizard);
